42f91e44e4a8c146335b00732d001b2c7963002a
Hasnain-Makhdoom <86113425+Hasnain-Makhdoom@users.noreply.github.com>
2024-05-07 11:31:43 +0500

    created task service to perform CRUD operations

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,35 @@
+// src/app/task.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Task } from '../models/task.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+  private apiUrl = 'http://localhost:8000/api/tasks'; // Replace with your Symfony API endpoint
+  // private apiUrl = 'https://quiet-scrubland-65890-672dc8da1147.herokuapp.com/api/tasks'; // Replace with your Symfony API endpoint
+
+  constructor(private http: HttpClient) { }
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  getTask(id: number): Observable<Task> {
+    return this.http.get<Task>(`${this.apiUrl}/${id}`);
+  }
+
+  createTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.apiUrl, task);
+  }
+
+  updateTask(task: Task): Observable<Task> {
+    return this.http.put<Task>(`${this.apiUrl}/${task.id}`, task);
+  }
+
+  deleteTask(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
